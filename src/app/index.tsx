b40b40434e352e307db5842e58b73dd4f07b1c9a
Akhilbabu456 <akhilbeliever001@gmail.com>
2025-01-17 23:17:06 +0530

    Custom components and customer auth ui done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { commonStyles } from "@/styles/commonStyles";
+import { splashStyles } from "@/styles/splashStyles";
+import CustomText from "@/components/shared/CustomText";
+import { useFonts } from "expo-font";
+import { resetAndNavigate } from "@/utils/Helpers";
 
 const Main = () => {
+  const [loaded] = useFonts({
+    Bold: require("@/assets/fonts/NotoSans-Bold.ttf"),
+    Regular: require("@/assets/fonts/NotoSans-Regular.ttf"),
+    Medium: require("@/assets/fonts/NotoSans-Medium.ttf"),
+    Light: require("@/assets/fonts/NotoSans-Light.ttf"),
+    SemiBold: require("@/assets/fonts/NotoSans-SemiBold.ttf"),
+  });
+
+  const [hasNavigated, setHasNavigated] = useState(false);
+
+  const tokenCheck = async () => {
+    resetAndNavigate("/role");
+  };
+
+  useEffect(() => {
+    if (loaded && !hasNavigated) {
+      const timeOutId = setTimeout(() => {
+        tokenCheck();
+        setHasNavigated(true);
+      }, 1000);
+      return () => clearTimeout(timeOutId);
+    }
+  }, [hasNavigated, loaded]);
+
   return (
-    <View>
-      <Text>Helo</Text>
+    <View style={commonStyles.container}>
+      <Image
+        source={require("@/assets/images/logo_t.png")}
+        style={splashStyles.img}
+      />
+
+      <CustomText variant="h5" fontFamily="Medium" style={splashStyles.text}>
+        Made in 🇮🇳
+      </CustomText>
     </View>
   );
 };
